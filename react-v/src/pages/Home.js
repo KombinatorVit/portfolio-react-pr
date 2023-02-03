3f0530fe2fc894a20d2c9bfcad52bc6c942bece2
KombinatorVit <slonik0775@gmail.com>
2023-02-03 23:58:11 +0200

    update

--- react-v/src/pages/Home.js
+++ react-v/src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
 						</li>
 						<li className="content-list__item">
 							<h2 className="title-2">Backend</h2>
-							<p>NodeJS, MySQL, MongoDB</p>
+							{/*<p>NodeJS, MySQL, MongoDB</p>*/}
 						</li>
 					</ul>
 				</div>
