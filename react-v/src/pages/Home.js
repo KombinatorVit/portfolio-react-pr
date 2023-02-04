38783fd6d0160a3db3574c981820cfa06879d62a
KombinatorVit <slonik0775@gmail.com>
2023-02-04 22:25:47 +0200

    update

--- react-v/src/pages/Home.js
+++ react-v/src/pages/Home.js
@@ -2,28 +2,28 @@ import Header from './../components/header/Header'
 
 const Home = () => {
     return (
-		<>
-			<Header />
-			<main className="section">
-				<div className="container">
-					<ul className="content-list">
-						<li className="content-list__item">
-							<h2 className="title-2">Frontend</h2>
-							<p>
-								JavaScript, TypeScript, ReactJS, Redux,
-								HTML, CSS, BootStrap, MaterialUI, Yarn,
-								TailwindCSS, StyledComponents
-							</p>
-						</li>
-						<li className="content-list__item">
-							<h2 className="title-2">Backend</h2>
-							{/*<p>NodeJS, MySQL, MongoDB</p>*/}
-						</li>
-					</ul>
-				</div>
-			</main>
-		</>
-	);
+        <>
+            <Header/>
+            <main className="section">
+                <div className="container">
+                    <ul className="content-list">
+                        <li className="content-list__item">
+                            <h2 className="title-2">Frontend</h2>
+                            <p>
+                                JavaScript, TypeScript, ReactJS, Redux,
+                                CSS, BootStrap, MaterialUI, Yarn,
+                                TailwindCSS, StyledComponents
+                            </p>
+                        </li>
+                        <li className="content-list__item">
+                            <h2 className="title-2">Backend</h2>
+                            <p>NodeJS, MySQL, MongoDB</p>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default Home;
